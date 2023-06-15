c4fc157e72b9cea9a32b44148b7d5d47d0381fbf
yasinldev <ozkayayasin964@gmail.com>
2023-06-15 18:21:04 +0300

    commit

--- static/mai-ui/js/components/_navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navbarToggle = document.querySelector('[data-mai-navbar-toggle]');
-    const navbarResponsive = document.querySelector('.navbar-responsive');
-
-    navbarToggle.addEventListener('click', function() {
-        navbarResponsive.classList.toggle('navbar-toggled');
-    });
-});
